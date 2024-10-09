5c37acc034470399dc48831c217356c5417e65cb
ZOE2K24 <jm.legaspi06@gmail.com>
2024-10-09 11:35:18 -0600

    4th update

--- tny_script.js
+++ tny_script.js
@@ -13,3 +13,19 @@
 /* Display the current date and time */
 
 document.getElementById("dateNow").innerHTML = "m/d/y<br />h:m:s";
+
+/* Display the time left until New Year's Eve */
+
+document.getElementById("days").textContent = "dd";
+document.getElementById("hrs").textContent = "hh";
+document.getElementById("mins").textContent = "mm";
+document.getElementById("secs").textContent = "ss";
+
+/* Store the current date and time */
+var currentDay = new Date("May 23, 2018 14:35:05");
+var dateStr = currentDay.toLocaleDateString();
+var timeStr = currentDay.toLocaleTimeString();
+
+/* Display the current date and time */
+document.getElementById("dateNow").innerHTML = dateStr + "<br />" + timeStr;
+
